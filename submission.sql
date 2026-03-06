e07664932e4fc2cbbd530bce7ab94490ca57da47
SUCHI1993 <suchismita.kundu@capgemini.com>
2026-03-06 18:00:50 +0530

    Update submission.sql

--- submission.sql
+++ submission.sql
@@ -1,14 +1,3 @@
----Customer dimension table
-
-CREATE TABLE CUSTOMER (
-customer_id   varchar,
-customer_email      varchar,
-customer_country    varchar,
-etl_loaded_at     timestampntz
-);
-
-
-
 ---fact table creation (assuming id values can be AlphaNumeric)
 CREATE TABLE FACT_ORDERS (
 order_id    varchar NOT NULL,
@@ -18,6 +7,8 @@ order_status   varchar,
 quantity   number,
 unit_price   number,
 currency   VARCHAR,
+customer_email      varchar,
+customer_country    varchar,
 source_updated_at timestampntz,
 etl_loaded_at     timestampntz,
 primary key (order_id, product_id, customer_id)
@@ -30,12 +21,12 @@ CREATE TEMP TABLE SOURCE_DETAILS_CDC (
 ORDER_ID,
 PRODUCT_ID,
 CUSTOMER_ID,
-CUSTOMER_EMAIL,
-CUSTOMER_COUNTRY,
 ORDER_STATUS,
 QUANTITY,
 UNIT_PRICE,
 CURRENCY,
+CUSTOMER_EMAIL,
+CUSTOMER_COUNTRY,
 SOURCE_UPDATED_AT,
 ETL_LOADED_AT
 );
@@ -45,10 +36,10 @@ ETL_LOADED_AT
 ----FULL LOAD/INITAL LOAD LOGIC
 
 
-INSERT INTO fact_orders (order_id,product_id, customer_id, order_status,currency,  quantity, unit_price, source_updated_at, etl_loaded_at)
+INSERT INTO fact_orders (order_id,product_id, customer_id, order_status,currency,  quantity, unit_price,customer_email,customer_country, source_updated_at, etl_loaded_at)
 
 SELECT
-	ORDER.order_id ,ORDER.product_id, ORDER.customer_id ,ORDER.order_status, ORDER.currency,  ORDER_ITM.quantity, ORDER_ITM.unit_price, ORDER.source_updated_at, ORDER.etl_loaded_at
+	ORDER.order_id ,ORDER.product_id, ORDER.customer_id ,ORDER.order_status, ORDER.currency,  ORDER_ITM.quantity, ORDER_ITM.unit_price, ORDER.customer_email,ORDER.customer_country,ORDER.source_updated_at, ORDER.etl_loaded_at
 
 FROM ORDER as ORDR
 INNER JOIN ORDER_ITEMS as ORDER_ITM on ORDER.order_id = ORDER_ITM.order_id ;
@@ -59,10 +50,10 @@ INNER JOIN ORDER_ITEMS as ORDER_ITM on ORDER.order_id = ORDER_ITM.order_id ;
 
 Step 1: 
 
-INSERT INTO SOURCE_DETAILS_CDC (order_id,product_id, customer_id, order_status,currency,  quantity, unit_price, source_updated_at, etl_loaded_at)
+INSERT INTO SOURCE_DETAILS_CDC (order_id,product_id, customer_id, order_status,currency,  quantity, unit_price,customer_email,customer_country, source_updated_at, etl_loaded_at)
 
 SELECT
-	ORDER.order_id ,ORDER.product_id, ORDER.customer_id ,ORDER.order_status, ORDER.currency,  ORDER_ITM.quantity, ORDER_ITM.unit_price, ORDER.source_updated_at, ORDER.etl_loaded_at
+	ORDER.order_id ,ORDER.product_id, ORDER.customer_id ,ORDER.order_status, ORDER.currency,  ORDER_ITM.quantity, ORDER_ITM.unit_price, ORDER.customer_email,ORDER.customer_country,ORDER.source_updated_at, ORDER.etl_loaded_at
 
 FROM ORDERS as ORDER
 INNER JOIN ORDER_ITEMS as ORDER_ITM on ORDER.order_id = ORDER.order_id
@@ -86,7 +77,7 @@ WHEN NOT MATCHED THEN
 
 ---FINAL QUERY TO BRING ALL REQUIRED ATTRIBUTES
 
-SELECT a.customer_id,c.customer_email,c.customer_country,a.order_id,p.product_name,p.category,p.brand,(o.quantity*o.unit_price) as revenue, o.currency, o.etl_loaded_at
+SELECT a.customer_id,o.customer_email,o.customer_country,a.order_id,p.product_name,p.category,p.brand,(o.quantity*o.unit_price) as revenue, o.currency, o.etl_loaded_at
 FROM fact_orders as o
 INNER JOIN products as p on o.product_id = p.product_id
-INNER JOIN CUSTOMERS as c on c.customer_id = o.customer_id;
+;
